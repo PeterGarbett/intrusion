cc5d830bef97ff77037d5c18e7ba1971ebf386f4
Peter Garbett <petegarbett1958@gmail.com>
2024-04-01 21:26:43 +0100

    pylint inspired modifications

--- yolo.py
+++ yolo.py
@@ -1,3 +1,5 @@
+'''
+
 #############################################
 # Object detection - YOLO - OpenCV
 # Author : Arun Ponnusamy   (July 16, 2018)
@@ -9,21 +11,26 @@
 # or on a file containing an image.  Direct command line
 # usage removed and yolo file locations fixed
 # at /etc/opt/yolo/
-#
 
-import cv2
+'''
+
+import sys
 import argparse
+import cv2
 import numpy as np
 
 
-def start(imageFileName, directImage, useDirect):
+def start(image_file_name, direct_image, use_direct):
+    '''
 
-    # This emulates what the software would expect for the command line.
-    # I've chosen /etc/opt/yolo as a home for the data files. This file
-    # on /usr/local/bin so I can call it froum anywhere
+    emulates what the software would expect for the command line.
+    I've chosen /etc/opt/yolo as a home for the data files. This file
+    on /usr/local/bin so I can call it froum anywhere
+
+    '''
 
     args = argparse.Namespace(
-        image=imageFileName,
+        image=image_file_name,
         config="/etc/opt/yolo/yolov3.cfg",
         weights="/etc/opt/yolo/yolov3.weights",
         classes="/etc/opt/yolo/yolov3.txt",
@@ -41,28 +48,28 @@ def start(imageFileName, directImage, useDirect):
 
         return output_layers
 
-    if useDirect:
-        image = directImage
+    if use_direct:
+        image = direct_image
     else:
         try:
             image = cv2.imread(args.image)
         except:
             print("Image file ", args.image, "not found")
-            exit()
+            sys.exit()
 
     try:
-        Width = image.shape[1]
-        Height = image.shape[0]
+        image_width = image.shape[1]
+        image_height = image.shape[0]
     except:
         return []
 
     scale = 0.00392
     classes = None
 
-    with open(args.classes, "r") as f:
-        classes = [line.strip() for line in f.readlines()]
+    with open(args.classes, "r",encoding='ascii') as class_file:
+        classes = [line.strip() for line in class_file.readlines()]
 
-    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    # COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
     net = cv2.dnn.readNet(args.weights, args.config)
 
@@ -72,27 +79,28 @@ def start(imageFileName, directImage, useDirect):
 
     outs = net.forward(get_output_layers(net))
 
+    #I don't actually uses these boxes at the moment  but they might be useful later
+
     class_ids = []
     confidences = []
     boxes = []
     conf_threshold = 0.5
-    nms_threshold = 0.4
 
     for out in outs:
         for detection in out:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5:
-                center_x = int(detection[0] * Width)
-                center_y = int(detection[1] * Height)
-                w = int(detection[2] * Width)
-                h = int(detection[3] * Height)
-                x = center_x - w / 2
-                y = center_y - h / 2
+            if confidence > conf_threshold:
+                center_x = int(detection[0] * image_width)
+                center_y = int(detection[1] * image_height)
+                box_width = int(detection[2] * image_width)
+                box_height = int(detection[3] * image_height)
+                box_x_coordinate = center_x - box_width / 2
+                box_y_coordinate = center_y - box_height / 2
                 class_ids.append(class_id)
                 confidences.append(float(confidence))
-                boxes.append([x, y, w, h])
+                boxes.append([box_x_coordinate, box_y_coordinate, box_width, box_height])
 
     items = list(set(class_ids))
     names = [str(classes[x]) for x in items]
@@ -100,12 +108,13 @@ def start(imageFileName, directImage, useDirect):
     return names
 
 
-def yoloImage(image):
+def yolo_image(image):
+    ''' Entry point for function to apply yolo to an image returning a list of found items '''
     items = start(None, image, True)
     return items
 
 
-def yoloFile(filename):
-
+def yolo_file(filename):
+    ''' Entry point for function to apply yolo to a file returning a list of found items '''
     items = start(filename, None, False)
     return items
