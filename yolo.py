b955cb52bd50d1bd501b17c2d5f4c51bc96174c8
Peter Garbett <petegarbett1958@gmail.com>
2024-04-09 13:39:16 +0100

    Modified for testability

--- yolo.py
+++ yolo.py
@@ -5,36 +5,37 @@
 # Author : Arun Ponnusamy   (July 16, 2018)
 # Website : http://www.arunponnusamy.com
 ############################################
-#
-# Hacked to return a list of types of objects found
-# and entry points added to operate directly on a image
-# or on a file containing an image.  Direct command line
-# usage removed and yolo file locations fixed
-# at /etc/opt/yolo/
+
+Hacked to interface to intrusion.py
 
 """
 
-import sys
-import argparse
 import cv2
 import numpy as np
+import glob
+
+config = "/etc/opt/yolo/yolov3.cfg"
+weights = "/etc/opt/yolo/yolov3.weights"
+classes_file = "/etc/opt/yolo/yolov3.txt"
 
 
-def start(image_file_name, direct_image, use_direct):
-    """
+def initialise_yolo():
+    """Separate out the file reading aspects to a once only read"""
+    global classes
+    global COLORS
+    global net
 
-    emulates what the software would expect for the command line.
-    I've chosen /etc/opt/yolo as a home for the data files. This file
-    on /usr/local/bin so I can call it froum anywhere
+    with open(classes_file, "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    net = cv2.dnn.readNet(weights, config)
 
-    """
 
-    args = argparse.Namespace(
-        image=image_file_name,
-        config="/etc/opt/yolo/yolov3.cfg",
-        weights="/etc/opt/yolo/yolov3.weights",
-        classes="/etc/opt/yolo/yolov3.txt",
-    )
+def yolo(image):
+    """use yolo on an image"""
+    global net
+
+    output_result_image = False
 
     def get_output_layers(net):
 
@@ -48,30 +49,20 @@ def start(image_file_name, direct_image, use_direct):
 
         return output_layers
 
-    if use_direct:
-        image = direct_image
-    else:
-        try:
-            image = cv2.imread(args.image)
-        except:
-            print("Image file ", args.image, "not found")
-            sys.exit()
+    def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
 
-    try:
-        image_width = image.shape[1]
-        image_height = image.shape[0]
-    except:
-        return []
+        label = str(classes[class_id])
+        color = COLORS[class_id]
 
+        cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+
+        cv2.putText(
+            img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2
+        )
+
+    Width = image.shape[1]
+    Height = image.shape[0]
     scale = 0.00392
-    classes = None
-
-    with open(args.classes, "r", encoding="ascii") as class_file:
-        classes = [line.strip() for line in class_file.readlines()]
-
-    # COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
-
-    net = cv2.dnn.readNet(args.weights, args.config)
 
     blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
 
@@ -79,39 +70,27 @@ def start(image_file_name, direct_image, use_direct):
 
     outs = net.forward(get_output_layers(net))
 
-    # I don't actually uses these boxes at the moment  but they might be useful later
-
     class_ids = []
     confidences = []
     boxes = []
     conf_threshold = 0.5
+    nms_threshold = 0.4
 
     for out in outs:
         for detection in out:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > conf_threshold:
-                center_x = int(detection[0] * image_width)
-                center_y = int(detection[1] * image_height)
-                box_width = int(detection[2] * image_width)
-                box_height = int(detection[3] * image_height)
-                box_x_coordinate = center_x - box_width / 2
-                box_y_coordinate = center_y - box_height / 2
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
                 class_ids.append(class_id)
                 confidences.append(float(confidence))
-                boxes.append(
-                    [box_x_coordinate, box_y_coordinate, box_width, box_height]
-                )
-
-
-#   Extract a list of the type of things we found
-
-    items = list(set(class_ids))
-    names = [str(classes[x]) for x in items]
-
-    conf_threshold = 0.5
-    nms_threshold = 0.4
+                boxes.append([x, y, w, h])
 
     indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
@@ -120,16 +99,56 @@ def start(image_file_name, direct_image, use_direct):
         things.append(classes[class_ids[i]])
     types = list(set(things))
 
+    if output_result_image:
+
+        for i in indices:
+            try:
+                box = boxes[i]
+            except:
+                i = i[0]
+                box = boxes[i]
+
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            draw_prediction(
+                image,
+                class_ids[i],
+                confidences[i],
+                round(x),
+                round(y),
+                round(x + w),
+                round(y + h),
+            )
+
+        cv2.imwrite("object-detection.jpg", image)
+        cv2.destroyAllWindows()
+
     return types
 
 
 def yolo_image(image):
-    """Entry point for function to apply yolo to an image returning a list of found items"""
-    items = start(None, image, True)
+    """Use yolo on an image and return what is found"""
+    items = yolo(image)
     return items
 
 
-def yolo_file(filename):
-    """Entry point for function to apply yolo to a file returning a list of found items"""
-    items = start(filename, None, False)
+def yolo_file(file_name):
+    """Use yolo on an image file and return what is found"""
+    image = cv2.imread(file_name)
+    items = yolo(image)
+
     return items
+
+
+if __name__ == "__main__":
+    """Test by using the routines on images in the current directory"""
+
+    imgnames = sorted(glob.glob("*.jpg"))
+
+    initialise_yolo()
+
+    for name in imgnames:
+        types = yolo_file(name)
+        print("Discovered :", types, " in ", name)
