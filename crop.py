3d2442d69163592c9a85ec927a1ecbeec5f493c9
Peter Garbett <petegarbett1958@gmail.com>
2024-06-30 22:35:02 +0100

    Remove size restriction. Add limit = None = take no action

--- crop.py
+++ crop.py
@@ -1,34 +1,32 @@
 import daytime
 
-# At some point set these up externally
-
-daycrop = (750, 750 + 640, 400, 400 + 480)
-nightcrop = (450, 1400, 480, 980)
-
 
 def crop(image, daylight, daycrop, nightcrop):
-    ''' Crop image in a daylight dependent way '''
+    """Crop image in a daylight dependent way"""
 
     size_x = image.shape[0]
     size_y = image.shape[1]
 
-    if size_x == 1080:
-        if daylight:
+    if daylight:
+
+        if daycrop is not None:
             x_low = daycrop[0]
             x_high = daycrop[1]
             y_low = daycrop[2]
             y_high = daycrop[3]
         else:
+            return image
+    else:
+        if nightcrop is not None:
             x_low = nightcrop[0]
             x_high = nightcrop[1]
             y_low = nightcrop[2]
             y_high = nightcrop[3]
-    else:
-        return image
+        else:
+            return image
 
-    # Cropping an HD image
+    # Cropping image
 
     cropped_image = image[y_low:y_high, x_low:x_high]
 
     return cropped_image
-
