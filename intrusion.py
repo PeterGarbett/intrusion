b0d3edc19c6912b2a56a7d0b1489adf5a5644a3d
Peter Garbett <petegarbett1958@gmail.com>
2024-07-01 21:21:26 +0100

    Correction to trigger value initialisation

--- intrusion.py
+++ intrusion.py
@@ -424,6 +424,7 @@ def generate(yolo_process_q, lock):
     daycrop = (750, 750 + 640, 400, 400 + 480)
     nightcrop = (450, 1400, 480, 980)
     location = (52.4823, -1.898575)  # Birmingham UK
+    trigger = sensitivityChange.value
 
     light = daytime.daytime(location)
 
